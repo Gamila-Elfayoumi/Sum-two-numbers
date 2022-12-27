6f3c5ba38365d051946d09e4207be5864c67fa10
Gamila-Elfayoumi <117528749+Gamila-Elfayoumi@users.noreply.github.com>
2022-12-27 21:02:50 +0200

    Add files via upload

---
+++ Sum of two numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, sum;
+	cout << "enter a \n";
+	cin >> a;
+	cout << "enter b \n";
+	cin >> b;
+	sum = a + b;
+	cout << a << "+" << b << "=" << sum << endl;
+	system("pause");
+}
